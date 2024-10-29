f73388d5af057f0ded4c0b1ad730480842e1e2b6
ChongShaoQi <SHAOQIS69.23@ichat.sp.edu.sg>
2024-10-29 13:15:26 +0800

    creating joke.js and main.js

---
+++ .github/actions/joke-action/joke.js
@@ -0,0 +1,29 @@
+const request = require("request-promise");
+
+const options = {
+  method: "GET",
+  uri: "https://icanhazdadjoke.com/",
+  headers: {
+    Accept: "application/json",
+    "User-Agent": "Writing JavaScript action GitHub Skills course.",
+  },
+  json: true,
+};
+
+async function getJoke() {
+  const res = await request(options);
+  return res.joke;
+}
+
+const getJoke = require("./joke");
+const core = require("@actions/core");
+
+async function run() {
+  const joke = await getJoke();
+  console.log(joke);
+  core.setOutput("joke-output", joke);
+}
+
+run();
+
+module.exports = getJoke;
